91bb712c911fbcf0e8feac49118f63fdfef0fa2f
Krossing95 <kalenuxoesekwasi@gmail.com>
2023-10-08 13:59:50 -0700

    Admin single equipment page redone

--- app/admin/equipment/[equipmentId]/page.tsx
+++ app/admin/equipment/[equipmentId]/page.tsx
@@ -5,6 +5,7 @@ import { Metadata, ResolvingMetadata } from 'next'
 import { notFound } from 'next/navigation'
 import * as React from 'react'
 import EquipmentPage from './components/EquipmentPage'
+import { cookies } from 'next/headers'
 
 type Props = {
     params: { equipmentId: string }
@@ -33,18 +34,27 @@ export async function generateMetadata(
 }
 export const dynamic = 'force-dynamic'
 
-const SingleEquipmentPage = async ({ params }: Props) => {
-    let data: Equipment
+const getEquipment = async ({ params }: Props) => {
+    const cookieStore = cookies()
+    const obj = cookieStore.get('__signedInUserObj')?.value || '{}'
+    const cookieObj = JSON.parse(obj)?.user
+    const token: string = cookieObj?.token
+    let equipment: Equipment
     try {
-        const getData = await getone_equipment({ equipment_id: params.equipmentId })
+        const getData = await getone_equipment({ equipment_id: params.equipmentId, token })
         if (parseInt(getData.data?.code) !== 200) notFound()
-        data = getData.data?.data
+        equipment = getData.data?.data
+        return equipment
     } catch (error) {
         notFound()
     }
+}
+
+const SingleEquipmentPage = async ({ params }: Props) => {
+    const equipment = await getEquipment({ params })
     return (
         <React.Suspense fallback={<SuspenseLoader text='Page is loading' ignoreOptionalHeight={false} />}>
-            <EquipmentPage data={data} />
+            <EquipmentPage data={equipment} />
         </React.Suspense>
     )
 }

f4f2e7aa5c190c075a09c5a5a1a521ee346f7278
Krossing95 <kalenuxoesekwasi@gmail.com>
2023-10-08 16:18:24 -0700

    recycle bin page created

--- app/admin/recycle_bin/page.tsx
+++ app/admin/recycle_bin/page.tsx
@@ -1,11 +1,22 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import * as React from 'react'
 
 const RecycleBinPage = () => {
     return (
         <Box component='div' sx={{ display: 'flex', height: 'calc(100vh - 400px)', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <Box component='div'>
-                <Typography gutterBottom variant='h6'>{'Select a folder below to view corresponding hidden items'}</Typography>
+            <Box component='div' sx={{ mb: 2 }}>
+                <Typography
+                    gutterBottom
+                    variant='h6'
+                    className='text'
+                    sx={{ textAlign: 'center' }}
+                >
+                    {'Select a folder below to view corresponding hidden items'}
+                </Typography>
+            </Box>
+            <Box>
+                <Button>folder</Button>
+                <Button>folder</Button>
             </Box>
         </Box>
     )

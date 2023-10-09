c2575a14dce0cd44b655370765082d941a2096b5
Krossing95 <kalenuxoesekwasi@gmail.com>
2023-10-09 16:26:47 -0700

    Recycle bin page created

--- app/admin/recycle_bin/page.tsx
+++ app/admin/recycle_bin/page.tsx
@@ -1,7 +1,9 @@
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, Tooltip, Typography } from '@mui/material'
+import { useRouter } from 'next/navigation'
 import * as React from 'react'
 
 const RecycleBinPage = () => {
+    const router = useRouter()
     return (
         <Box component='div' sx={{ display: 'flex', height: 'calc(100vh - 400px)', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
             <Box component='div' sx={{ mb: 2 }}>
@@ -15,8 +17,18 @@ const RecycleBinPage = () => {
                 </Typography>
             </Box>
             <Box>
-                <Button>folder</Button>
-                <Button>folder</Button>
+                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                    <Button onClick={() => router.push('/recycle_bin/equipment')}>
+                        <img src='/images/folder.webp' alt='Folder graphics' width={50} height={50} />
+                    </Button>
+                    <Typography>{"Equipment folder"}</Typography>
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                    <Button onClick={() => router.push('/recycle_bin/bookings')}>
+                        <img src='/images/folder.webp' alt='Folder graphics' width={50} height={50} />
+                    </Button>
+                    <Typography>{"Bookings folder"}</Typography>
+                </Box>
             </Box>
         </Box>
     )
